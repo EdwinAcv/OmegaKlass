6d5d860a152b45e6a3f5c4e515b418a5998b6580
Georges Gil <georgesgil22@gmail.com>
2024-08-04 20:27:32 -0400

    otro dia

--- src/components/profesores/TeacherList.tsx
+++ src/components/profesores/TeacherList.tsx
@@ -24,7 +24,8 @@ export const TeacherList: React.FC<TeacherListProps> = ({ teachers }) => {
               <p className="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">{teacher.tutorias ? 'Disponible para tutorías' : 'No disponible para tutorías'}</p>
             </div>
             {/* <h1 className="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">{teacher.materia.join(', ')}</h1> */}
-            <div className="flex gap-2">
+            <img className="mt-4 h-48 w-full object-cover object-center" src={teacher.imagen || ''} alt={teacher.nombre} />
+            <div className="flex flex-wrap gap-2">
               {teacher.materia.map((materia, index) => (
                 <div key={index} className="center relative inline-block select-none whitespace-nowrap rounded-lg bg-blue-500 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
                   <div className="mt-px">{materia}</div>
@@ -33,13 +34,13 @@ export const TeacherList: React.FC<TeacherListProps> = ({ teachers }) => {
             </div>
             <div className="my-4">
               <p>{teacher.descipcion}</p>
-              <h2 className="mt-4 text-gray-800 text-xl font-bold cursor-pointer">Sub habilidades:</h2>
+              <h2 className="mt-4 text-gray-800 text-xl font-bold cursor-pointer">Habilidades:</h2>
               <ul>
-              {teacher.sub_abilidades.map((skill, index) => (
-  <div key={index} className="center relative inline-flex items-center justify-center m-2 whitespace-nowrap rounded-lg bg-blue-500 py-1 px-2 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
-    <div className="mt-px">{skill}</div>
-  </div>
-))}
+                {teacher.sub_abilidades.map((skill, index) => (
+                  <div key={index} className="center relative inline-flex items-center justify-center m-2 whitespace-nowrap rounded-lg bg-blue-500 py-1 px-2 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
+                    <div className="mt-px">{skill}</div>
+                  </div>
+                ))}
               </ul>
             </div>
           </div>
